5d6e4889f1d16a6580646749e28c9586626be3f2
destinyjob <jobdestiny4@gmail.com>
2022-01-31 12:20:43 +0100

    New files

---
+++ conditionals.dart
@@ -0,0 +1,48 @@
+void main() {
+  //Conditional statements
+  // if(condition){
+  //   //Statements will execute if the condition evaluates to true
+  // }
+
+  // int first = 90;
+  // int second = 78;
+
+  // if (first == second) {
+  //   print("It's a tie");
+  // }
+
+  // if (first > second) {
+  //   print("It's a tie");
+  // }
+
+  // print("This is just a print statement");
+
+//  if(condition){
+//     //Statements will execute if the condition evaluates to true
+//   }else{
+//     //Statements will execute if the condition evaluates to false
+//   }
+
+  // int score = 45;
+  // int benchmark = 45;
+
+  // if (score == benchmark) {
+  //   print("You passed");
+  // } else {
+  //   print("Do better next time");
+  // }
+
+  //Else if
+  // bool isRaining = true;
+  // bool isSunny = true;
+
+  // if (isRaining && !isSunny) {
+  //   print("There is no rainbow outside");
+  // } else if (!isRaining && isSunny) {
+  //   print("Go out on any moderate dress");
+  // } else if (!isRaining || isSunny) {
+  //   print("Turn on the AC");
+  // } else {
+  //   print("Hope you didn't get confused");
+  // }
+}
---
+++ terna.dart
@@ -0,0 +1,28 @@
+void main() {
+  //Ternary operators
+  // condition ? expression1 : expression2
+
+  String country = "UAE";
+  int age = 22;
+  int amount = 5000;
+  //                            true                              false
+  country == "Nigeria" ? print('Unpatriotic') : print('Patriotic citizen!');
+
+  // amount == age
+  //     ? print('This is false')
+  //     : amount == age
+  //         ? print('This is Obinna')
+  //         : amount == age
+  //             ? print('This is true')
+  //             : amount == age
+  //                 ? print('This is another')
+  //                 : print('Finally');
+
+  // if (amount == age) {
+  //   print('This is false');
+  // } else if (amount == age) {
+  //   print('This is obinna');
+  // } else {
+  //   print('This is not the way');
+  // }
+}
