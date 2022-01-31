0d72279ad745961367f089afaa14aa32bc9cd98c
destinyjob <jobdestiny4@gmail.com>
2022-01-31 10:59:27 +0100

    Deebug Flutter Ekpooma Classes

---
+++ concatEtInterpolate.dart
@@ -0,0 +1,14 @@
+void main() {
+  //Concantenation
+  //This allows you to add two strings together using the addition symbol
+
+  // String age = '23';
+  // String name = 'Destiny';
+  // print('I am ' + age + ' years old');
+  // print('My name is ' + name + ' and i am ' + age + ' years old');
+  //INTERPOLATION
+  // int age = 25;
+  // int years = 2022;
+  // print('I am $age years old');
+  // print('I am ${age + age} years old');
+}
---
+++ firstclass.dart
@@ -0,0 +1,21 @@
+void main() {
+  //data-type  identifier  assignment  value
+  // declaration and initialized
+
+  //declaration
+  //int score;
+  //initialize
+  //score = 45;
+
+  //int 0age;//You cannot start your variable with a number
+  //double %pi;//You cannot start your variable with a symbol
+  //String _name;
+  //bool is_light_on;//best naming convention
+
+  int age = 100;
+  double pi = 3.142;
+  String name = "Destiny";
+  bool is_light_on = true;
+
+  print(age);
+}
