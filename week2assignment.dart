55993bed16a3432ddba88861936f21e5223e5686
destinyjob <jobdestiny4@gmail.com>
2022-02-02 13:48:07 +0100

    assignment

---
+++ week2assignment.dart
@@ -0,0 +1,41 @@
+void main() {
+  /*
+  WEEK 2 ASSESSMENT
+
+>> Activity 1
+Write a program to check if the value of a variable is an even number or not
+
+
+>> Activity 2
+You have just finished building a physics calculator app for a client.
+One of the features of this calculator is to solve for Acceleration using this formula:
+Force = Mass x Acceleration.
+Now, let us test if it works as the client would want it.
+Given:
+Force = 25.5N
+Mass = 10.2kg
+Write an algorithm to solve for acceleration.
+NOTE: Ensure that the final result is an integer whole number
+
+
+>> Activity 3
+Write a program to collect 3 different numbers from the user and print the greatest among the three
+
+
+
+>> Activity 4
+Answer the question (i) using ternary operator for your conditional statement.
+
+
+
+>> Activity 5
+Write a program that collects user input and check if the users input is positive or negative
+
+NOTE: Push the entire project folder containing the files you answered each activity to your Github repository and add me to the project as a collaborator.
+
+My Github username: destinyjob
+
+If you have any questions, please ask. But digest the assignment properly before asking.
+
+   */
+}
