773e4983a465b35c79f0495a3a303fc239b663f7
destinyjob <jobdestiny4@gmail.com>
2022-02-17 17:27:09 +0100

    Re-arranged files

---
+++ Raw class Files/funct2.dart
@@ -0,0 +1,47 @@
+// void main() {
+//   // add(2, 2.5);
+//   // subtract(two: 2.5, one: 2);
+
+//   mutiply(5, 4);
+//   mutiply(5, 4, 3);
+//   divide(4, 5, 8);
+//   modulus(2, b: 3, c: 2);
+// }
+
+// //Fucntion with positional parameters
+// void add(int one, double two) {
+//   print(one + two);
+// }
+
+// //Function with named parameters
+// void subtract({int? one, double? two}) {
+//   print(one! + two!);
+// }
+
+// //Fuction with a positional optional parameter
+// void mutiply(int x, int y, [int? z]) {
+//   int sum = x + y;
+//   if (z != null) {
+//     sum += z;
+//   } else {
+//     print("The value of z is: $z");
+//   }
+//   print(sum);
+// }
+
+// //Function with default parameters
+// void divide(int x, int y, [int z = 7]) {
+//   int sum = x + y + z;
+//   print(sum);
+// }
+
+// //Function with a named parameter and a positional parameter
+// void modulus(int a, {int? b, int? c}) {
+//   print(a + b! + c!);
+// }
+
+void main() {
+  String anything = '1. Destiny';
+
+  print(anything.substring(4));
+}
